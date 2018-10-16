6ee99c8be502b35bc0bcfb0903edd67acb6ad7eb
Milan Urosevic <kobimilan995@yahoo.com>
2018-10-16 15:15:19 +0200

    front end pagination for users

--- src/components/admin/Users.vue
+++ src/components/admin/Users.vue
@@ -12,22 +12,34 @@
         <div v-else style="align-content: center;">
             <i class="fa fa-spinner fa-spin" style="font-size:48px"></i>
         </div>
+
+        <div class="pagination" style="margin-top: 50px;" v-if="lastPage > 1">
+            <a href="#" @click.prevent="paginateUsers(currentPage - 1)">&laquo;</a>
+            <a href="#" v-for="n in lastPage" :class="{'active': n == currentPage}" @click.prevent="paginateUsers(n)">{{n}}</a>
+            <a href="#" @click.prevent="paginateUsers(currentPage + 1)">&raquo;</a>
+        </div>
     </div>
 
 </template>
 
 <script>
 import User from './User.vue';
+import Paginate from 'vuejs-paginate';
 export default {
     data() {
         return {
             users: [],
             search_query: '',
-            loading: false
+            loading: false,
+            currentPage: 1,
+            usersCount: 0,
+            lastPage: 0,
+            perPage: 4
         }
     },
     components: {
-        'user-component': User
+        'user-component': User,
+        'pagination-component': Paginate
     },
     created() {
         this.fetchUsers();
@@ -36,13 +48,28 @@ export default {
     methods: {
         fetchUsers() {
             this.loading = true;
-            axios.get(window.api_url+'/api/admin/users?query='+this.search_query).then(response => {
-                // console.log(response.data);
+            axios.get(window.api_url+'/api/admin/users?query='+this.search_query+'&current_page='+this.currentPage+'&per_page='+this.perPage).then(response => {
+                console.log(response.data);
+                this.usersCount = response.data.count.count;
+                this.lastPage = Math.ceil(this.usersCount / this.perPage);
                 this.users = response.data.users;
                 this.loading = false;
             }).catch(error => {
                 console.log(error);
             });
+        },
+
+
+        paginateUsers(page) {
+            if(page == this.lastPage + 1) {
+                return;
+            }
+
+            if(page == 0) {
+                return;
+            }
+            this.currentPage = page;
+            this.fetchUsers();
         },
 
         removeUser(payload) {
@@ -57,3 +84,23 @@ export default {
     }
 }
 </script>
+
+<style>
+/* Pagination links */
+.pagination a {
+    color: black;
+    float: left;
+    padding: 8px 16px;
+    text-decoration: none;
+    transition: background-color .3s;
+}
+
+/* Style the active/current link */
+.pagination a.active {
+    background-color: dodgerblue;
+    color: white;
+}
+
+/* Add a grey background color on mouse-over */
+.pagination a:hover:not(.active) {background-color: #ddd;}
+</style>
